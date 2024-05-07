531e752082f66e7904393b7fd3d1795536a27088
Furkan Detseli <68570706+furkandetseli@users.noreply.github.com>
2024-05-07 20:15:44 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,67 +1,16 @@
-// Görselleri göstermek için fonksiyonlar
-function Yazdir(){
-    document.getElementById("result").innerHTML=`<img width="90%" height="60%"  src='./görseller/bavul.jpg'>`;
-}
-function Yazdir2(){
-    document.getElementById("result").innerHTML=`<img width="90%" height="60%"  src='./görseller/kelmacit.jpeg'>`;
-}
-function Yazdir3(){
-    document.getElementById("result").innerHTML=`<img width="90%" height="60%"  src='./görseller/han.jpg'>`;
-}
-function Yazdir4(){
-    document.getElementById("result").innerHTML=`<img width="90%" height="60%" src='./görseller/dedemuzver.jpeg'>`;
-}
-function Yazdir5(){
-    document.getElementById("result").innerHTML=`<img width="90%" height="60%"  src='./görseller/formula.jpg'>`;
-}
-function Yazdir6(){
-    document.getElementById("result").innerHTML=`<img width="90%" height="60%"  src='./görseller/mordeve.jpeg'>`;
-}
-function Yazdir7(){
-    document.getElementById("result").innerHTML=`<img width="90%" height="60%"  src='./görseller/cıvıkyumurta.jpeg'>`;
-}
-function Yazdir8(){
-    document.getElementById("result").innerHTML=`<img width="90%" height="60%" src='./görseller/tamek.jpeg'>`;
-}
-function Yazdir9(){
-    document.getElementById("result").innerHTML=`<img width="90%" height="60%" src='./görseller/tolga.jpg'>`;
-}
-function Yazdir10(){
-    document.getElementById("result").innerHTML="";
-}
-
-// Butonlara tıklama olaylarını atama
-var btnYaz1=document.getElementById("btn-1");
-btnYaz1.onclick=Yazdir;
-
-var btnYaz2=document.getElementById("btn-2");
-btnYaz2.onclick=Yazdir2;
-
-var btnYaz3=document.getElementById("btn-3");
-btnYaz3.onclick=Yazdir3;
-
-var btnYaz4=document.getElementById("btn-4");
-btnYaz4.onclick=Yazdir4;
-
-var btnYaz5=document.getElementById("btn-5");
-btnYaz5.onclick=Yazdir5;
-
-var btnYaz6=document.getElementById("btn-6");
-btnYaz6.onclick=Yazdir6;
-
-var btnYaz7=document.getElementById("btn-7");
-btnYaz7.onclick=Yazdir7;
-
-var btnYaz8=document.getElementById("btn-8");
-btnYaz8.onclick=Yazdir8;
-
-var btnYaz9=document.getElementById("btn-9");
-btnYaz9.onclick=Yazdir9;
-
-var btnYaz10=document.getElementById("btn-10");
-btnYaz10.onclick=Yazdir10;
-
 // Navbar rengini değiştirmek için fonksiyon
 function changeColor(color) {
     document.querySelector('.navbar').style.backgroundColor = color;
 }
+
+// Görselleri göstermek için fonksiyonlar
+function Yazdir(){
+    document.getElementById("result").innerHTML=`<img width="90%" height="60%"  src='./görseller/bavul.jpg'>`;
+}
+// Diğer Yazdir fonksiyonları...
+
+// Butonlara tıklama olaylarını atama
+var btnYaz1=document.getElementById("btn-1");
+btnYaz1.onclick=Yazdir;
+// Diğer buton olay atamaları...
+
