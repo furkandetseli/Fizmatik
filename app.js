50f8b33b416a9e4971b399fcdeb3ee385a481c4b
Furkan Detseli <68570706+furkandetseli@users.noreply.github.com>
2024-05-07 22:01:41 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,60 +1,104 @@
-function Yazdir(){
-    document.getElementById("result").innerHTML=`<img width="90%" height="60%"  src='./görseller/bavul.jpg'>`;
-}
-function Yazdir2(){
-    document.getElementById("result").innerHTML=`<img width="90%" height="60%"  src='./görseller/kelmacit.jpeg'>`;
-}
-function Yazdir3(){
-    document.getElementById("result").innerHTML=`<img width="90%" height="60%"  src='./görseller/han.jpg'>`;
-}
-function Yazdir4(){
-    document.getElementById("result").innerHTML=`<img width="90%" height="60%" src='./görseller/dedemuzver.jpeg'>`;
-}
-function Yazdir5(){
-    document.getElementById("result").innerHTML=`<img width="90%" height="60%"  src='./görseller/formula.jpg'>`;
-}
-function Yazdir6(){
-    document.getElementById("result").innerHTML=`<img width="90%" height="60%"  src='./görseller/mordeve.jpeg'>`;
-}
-function Yazdir7(){
-    document.getElementById("result").innerHTML=`<img width="90%" height="60%"  src='./görseller/cıvıkyumurta.jpeg'>`;
-}
-function Yazdir8(){
-    document.getElementById("result").innerHTML=`<img width="90%" height="60%" src='./görseller/tamek.jpeg'>`;
-}
-function Yazdir9(){
-    document.getElementById("result").innerHTML=`<img width="90%" height="60%" src='./görseller/tolga.jpg'>`;
-}
-function Yazdir10(){
-    document.getElementById("result").innerHTML="";
+
+const fizikFormuller = [
+    { ad: "Fiziğin Alt Dalları", resim: "./görseller/kamyonet.jpg" },
+    { ad: "Temel Büyüklükler", resim: "./görseller/kısamuz.jpg" },
+    { ad: "Yoğunluk", resim: "./görseller/yoğunluk.jpg" },
+    { ad: "Kinetik Enerji", resim: "./görseller/kinetik.jpg" },
+    { ad: "Çekim Potansiyel Enerjisi", resim: "./görseller/morguvercin.jpg" },
+    { ad: "Sıvılarda Basınç", resim: "./görseller/sivibasinc.jpg" },
+    { ad: "Kapalı Kaplardaki Basınç", resim: "./görseller/pvnrt.jpg" },
+    { ad: "Sürtünme Kuvveti", resim: "./görseller/kene.jpg" },
+    { ad: "Atışlar, Serbest Düşme, Sürtünmeli Durum", resim: "./görseller/kosova.jpg" },
+    { ad: "İvme", resim: "./görseller/ivme.jpg" },
+    // Diğer formüller ve resimler
+];
+const kimyaFormuller = [
+    { ad: "İdeal Gaz Yasası", resim: "./görseller/gaz.jpg" },
+    { ad: "1A Grubu Elementleri", resim: "./görseller/1a.jpg" },
+    { ad: "2A Grubu Elementleri", resim: "./görseller/2a.jpg" },
+    { ad: "6A Grubu Elementleri:", resim: "./görseller/a6.jpg" },
+    { ad: "Periyodik Cetvelde Değişen Özelliklerin Artış Yönleri", resim: "./görseller/acemi.jpg" },
+    { ad: "Sıvılarda Basınç", resim: "./görseller/sivibasinc.jpg" },
+    // Diğer formüller ve resimler
+];
+
+const biyolojiFormuller = [
+    { ad: "Eşeysiz Üreme Şekilleri", resim: "./görseller/kamyonet.jpg" },
+    { ad: "Disakkaritler", resim: "./görseller/kısamuz.jpg" },
+    { ad: "Fotosentezde Devirli Fosforilasyon", resim: "./görseller/yoğunluk.jpg" },
+    { ad: "Canlıların Sınıflandırılması", resim: "./görseller/kinetik.jpg" },
+    { ad: "Bölünme Gerçekleştirmeyen Hücreler", resim: "./görseller/morguvercin.jpg" },
+    { ad: "Hücre Bölünme Sırası", resim: "./görseller/sivibasinc.jpg" },
+    { ad: "ATP’nin Harcandığı Olaylar", resim: "./görseller/kabes.png" },
+    { ad: "Hücre Çeperi Bulunduran Canlılar", resim: "./görseller/kene.jpg" },
+    // Diğer formüller ve resimler
+];
+
+var secilenFormul = null;
+
+var btnFizik = document.getElementById("fizik-buton");
+btnFizik.onclick = function() {
+    if (secilenFormul !== null && fizikFormuller.includes(secilenFormul)) {
+        formulleriGoster(fizikFormuller);
+    }
+};
+
+var btnKimya = document.getElementById("kimya-buton");
+btnKimya.onclick = function() {
+    if (secilenFormul !== null && kimyaFormuller.includes(secilenFormul)) {
+        formulleriGoster(kimyaFormuller);
+    }
+};
+
+var btnBiyoloji = document.getElementById("biyoloji-buton");
+btnBiyoloji.onclick = function() {
+    formulleriGoster(biyolojiFormuller);
+};
+
+
+function formulleriGoster(formulListesi) {
+    let formullerHTML = "";
+    formulListesi.forEach((formul, index) => {
+        formullerHTML += `<button id="btn-${index + 1}" class="btn-${index + 1}">${formul.ad}</button>`;
+    });
+    document.querySelector(".buttons").innerHTML = formullerHTML;
+
+    document.querySelector(".buttons").addEventListener("click", function(event) {
+        if (event.target.tagName === "BUTTON") {
+            let btnIndex = parseInt(event.target.id.replace("btn-", ""));
+            secilenFormul = formulListesi[btnIndex - 1]; // Doğru kullanım
+            document.getElementById("result").innerHTML = `<img width="90%" height="60%" src='${secilenFormul.resim}'>`;
+        }
+    });
 }
 
-var btnYaz1=document.getElementById("btn-1");
-btnYaz1.onclick=Yazdir;
+var btnFizik = document.getElementById("fizik-buton");
+btnFizik.onclick = function() {
+    formulleriGoster(fizikFormuller);
+};
 
-var btnYaz2=document.getElementById("btn-2");
-btnYaz2.onclick=Yazdir2;
+var btnKimya = document.getElementById("kimya-buton");
+btnKimya.onclick = function() {
+    formulleriGoster(kimyaFormuller);
+};
 
-var btnYaz3=document.getElementById("btn-3");
-btnYaz3.onclick=Yazdir3;
+var btnBiyoloji = document.getElementById("biyoloji-buton");
+btnBiyoloji.onclick = function() {
+    formulleriGoster(biyolojiFormuller);
+};
 
-var btnYaz4=document.getElementById("btn-4");
-btnYaz4.onclick=Yazdir4;
+function formulleriGoster(formulListesi) {
+    let formullerHTML = "";
+    formulListesi.forEach((formul, index) => {
+        formullerHTML += `<button id="btn-${index + 1}" class="btn-${index + 1}">${formul.ad}</button>`;
+    });
+    document.querySelector(".buttons").innerHTML = formullerHTML;
 
-var btnYaz5=document.getElementById("btn-5");
-btnYaz5.onclick=Yazdir5;
-
-var btnYaz6=document.getElementById("btn-6");
-btnYaz6.onclick=Yazdir6;
-
-var btnYaz7=document.getElementById("btn-7");
-btnYaz7.onclick=Yazdir7;
-
-var btnYaz8=document.getElementById("btn-8");
-btnYaz8.onclick=Yazdir8;
-
-var btnYaz9=document.getElementById("btn-9");
-btnYaz9.onclick=Yazdir9;
-
-var btnYaz10=document.getElementById("btn-10");
-btnYaz10.onclick=Yazdir10;
+    document.querySelector(".buttons").addEventListener("click", function(event) {
+        if (event.target.tagName === "BUTTON") {
+            let btnIndex = parseInt(event.target.id.replace("btn-", ""));
+            secilenFormul = formulListesi[btnIndex - 1];
+            document.getElementById("result").innerHTML = `<img width="90%" height="60%" src='${secilenFormul.resim}'>`;
+        }
+    });
+}
