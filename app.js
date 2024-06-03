f2f630d9f058fad5c1ff38dc226862c054666599
Furkan Detseli <68570706+furkandetseli@users.noreply.github.com>
2024-06-03 22:47:03 +0300

    geniş çaplı güncelleme!
    js komutları sadeleştirildi, yeni font eklendi. Güncellenmiş görseller ile sitenin işleyişi ve tasarımı daha alıcı hale getirilirken.

--- app.js
+++ app.js
@@ -1,15 +1,26 @@
+const fullscreenImage = document.querySelector('.fullscreen-image');
+
+fullscreenImage.addEventListener('click', () => {
+    fullscreenImage.style.transition = 'opacity 4s ease'; // Geçiş efekti süresi
+    fullscreenImage.style.opacity = '1'; // Yavaşça görünmez yap
+    fullscreenImage.style.display = 'none';
+});
 
 const fizikFormuller = [
     { ad: "Fiziğin Alt Dalları", resim: "./görseller/kamyonet.png" },
     { ad: "Temel Büyüklükler", resim: "./görseller/kısamuz.png" },
-    { ad: "Yoğunluk", resim: "./görseller/dedemuzver.jpeg" },
+    { ad: "Yoğunluk", resim: "./görseller/dedemuzver.png" },
     { ad: "Kinetik Enerji", resim: "./görseller/mavikare.png" },
     { ad: "Çekim Potansiyel Enerjisi", resim: "./görseller/morgüvercin.png" },
     { ad: "Sıvılarda Basınç", resim: "./görseller/hadigari.png" },
-    { ad: "Kapalı Kaplardaki Basınç", resim: "./görseller/paraver.png" },
+    { ad: "Kapalı Kaplardaki Basınç", resim: "./görseller/paraver1.png" },
     { ad: "Sürtünme Kuvveti", resim: "./görseller/kene.png" },
-    { ad: "Atışlar, Serbest Düşme, Sürtünmeli Durum", resim: "./görseller/kosova.png" },
-    { ad: "İvme", resim: "./görseller/damat.png" },
+    { ad: "Kapalı Kaplardaki Basınç", resim: "./görseller/paraver1.png" },
+    { ad: "Hava Direnci Kuvveti", resim: "./görseller/kosova.png" },
+    { ad: "Manyetik Tork", resim: "./görseller/abi.png" },
+    { ad: "Sıcaklık Değişimi", resim: "./görseller/kelmacit.png" },
+    { ad: "Hal Değişimi", resim: "./görseller/emel.png" },
+    { ad: "Yüklü Parçacıkların Manyetik Alan İçindeki Hareketi", resim: "./görseller/zeynep.png" },
     // Diğer formüller ve resimler
 ];
 const kimyaFormuller = [
@@ -18,18 +29,19 @@ const kimyaFormuller = [
     { ad: "2A Grubu Elementleri", resim: "./görseller/2a.png" },
     { ad: "6A Grubu Elementleri:", resim: "./görseller/a6.png" },
     { ad: "Periyodik Cetvelde Değişen Özelliklerin Artış Yönleri", resim: "./görseller/acemi.png" },
+    { ad: "Amofer Metaller", resim: "./görseller/a6.png" },
     // Diğer formüller ve resimler
 ];
 
 const biyolojiFormuller = [
-    { ad: "Eşeysiz Üreme Şekilleri", resim: "./görseller/1.png" },
-    { ad: "Disakkaritler", resim: "./görseller/2.png" },
-    { ad: "Fotosentezde Devirli Fosforilasyon", resim: "./görseller/3.png" },
-    { ad: "Canlıların Sınıflandırılması", resim: "./görseller/4.png" },
-    { ad: "Bölünme Gerçekleştirmeyen Hücreler", resim: "./görseller/5.png" },
-    { ad: "Hücre Bölünme Sırası", resim: "./görseller/6.png" },
+    { ad: "Eşeysiz Üreme Şekilleri", resim: "./görseller/bitiverse.png" },
+    { ad: "Disakkaritler", resim: "./görseller/masal.png" },
+    { ad: "Fotosentezde Devirli Fosforilasyon", resim: "./görseller/kafapası.png" },
+    { ad: "Canlıların Sınıflandırılması", resim: "./görseller/türkiye.png" },
+    { ad: "Bölünme Gerçekleştirmeyen Hücreler", resim: "./görseller/kars.png" },
+    { ad: "Hücre Bölünme Sırası", resim: "./görseller/ipmat.png" },
     { ad: "ATP’nin Harcandığı Olaylar", resim: "./görseller/kabes.png" },
-    { ad: "Hücre Çeperi Bulunduran Canlılar", resim: "./görseller/7.png" },
+    { ad: "Hücre Çeperi Bulunduran Canlılar", resim: "./görseller/babam.png" },
     // Diğer formüller ve resimler
 ];
 
@@ -54,23 +66,6 @@ btnBiyoloji.onclick = function() {
     formulleriGoster(biyolojiFormuller);
 };
 
-
-function formulleriGoster(formulListesi) {
-    let formullerHTML = "";
-    formulListesi.forEach((formul, index) => {
-        formullerHTML += `<button id="btn-${index + 1}" class="btn-${index + 1}">${formul.ad}</button>`;
-    });
-    document.querySelector(".buttons").innerHTML = formullerHTML;
-
-    document.querySelector(".buttons").addEventListener("click", function(event) {
-        if (event.target.tagName === "BUTTON") {
-            let btnIndex = parseInt(event.target.id.replace("btn-", ""));
-            secilenFormul = formulListesi[btnIndex - 1]; // Doğru kullanım
-            document.getElementById("result").innerHTML = `<img width="90%" height="90%" src='${secilenFormul.resim}'>`;
-        }
-    });
-}
-
 var btnFizik = document.getElementById("fizik-buton");
 btnFizik.onclick = function() {
     formulleriGoster(fizikFormuller);
@@ -89,7 +84,7 @@ btnBiyoloji.onclick = function() {
 function formulleriGoster(formulListesi) {
     let formullerHTML = "";
     formulListesi.forEach((formul, index) => {
-        formullerHTML += `<button id="btn-${index + 1}" class="btn-${index + 1}">${formul.ad}</button>`;
+        formullerHTML += `<button id="btn-${index + 1}" class="btn-biyoloji">${formul.ad}</button>`;
     });
     document.querySelector(".buttons").innerHTML = formullerHTML;
 
@@ -97,7 +92,8 @@ function formulleriGoster(formulListesi) {
         if (event.target.tagName === "BUTTON") {
             let btnIndex = parseInt(event.target.id.replace("btn-", ""));
             secilenFormul = formulListesi[btnIndex - 1];
-            document.getElementById("result").innerHTML = `<img width="90%" height="90%" src='${secilenFormul.resim}'>`;
+            secilenFormul = formulListesi[btnIndex - 1];
+            document.getElementById("result").innerHTML = `<img width="100%" height="100%" src='${secilenFormul.resim}'>`;
         }
     });
 }
